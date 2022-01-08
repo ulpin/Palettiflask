821923d0367a078e62498ec0a21fef15a6a396bd
__Chris <accnts@pm.me>
2022-01-08 09:29:50 +0100

    initial

---
+++ README.md
@@ -0,0 +1,6 @@
+# API to control LEDWAll of piTV
+
+# REQs
+# V1.0 Just execute single sudo ./go 1
+
+# Implement function to turn off again.
---
+++ main.py
@@ -0,0 +1,12 @@
+from flask import Flask
+from flask_restful import Api, Resource, reqparse, abort, fields, marshal_with
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+api = Api(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+db = SQLAlchemy(app)
+
+
+if __name__ == "__main__":
+	app.run(debug=True)
